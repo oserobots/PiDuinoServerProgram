5ea32ebe9b3048c1dfdd69a4245d969fd702a5c6
Senobio V. Chavez <Toriama@users.noreply.github.com>
2017-09-07 13:07:22 -0400

    updated setup instructions modified Upload.php to accept only text, doc, and sh files

--- Upload.php
+++ Upload.php
@@ -2,20 +2,20 @@
 $target_dir = "awesome/r/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-// Check if image file is a actual image or fake image
+// Check if text file is a actual text or fake text file
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = gettext($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".\n";
+        echo "File is text.\n";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.\n";
+        echo "File is not text.\n";
         $uploadOk = 0;
     }
 }
-// Check if file already exists
+// Check if file exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.\n";
     $uploadOk = 0;
@@ -26,9 +26,8 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.\n";
+if($textFileType != "txt" && $textFileType != "sh" && $textFileType != "doc" ) {
+    echo "Sorry, only txt, doc & sh files are allowed.\n";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
